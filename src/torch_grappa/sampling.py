fix sim masking function, add auto-segmentation of calibration region

src/torch_grappa/sampling.py
@@ -6,7 +6,82 @@ from typing import Tuple
 
 import torch
 
-# TODO: function to remove calibration region from data
+
+def segment_calibration(data: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, slice]:
+    """
+    Automatic segmentation of calibration data from k-space.
+
+    Parameters
+    ----------
+    data : torch.Tensor
+        K-space data with shape (C, *im_size), where N is the number of samples,
+        C is the number of coils, and im_size is the size of the image in each dimension.
+    return_slices : bool, optional
+        If True, return the slices used for calibration, by default False.
+
+    Returns
+    -------
+    calib : torch.Tensor
+        Calibration data extracted from the k-space data.
+    calib_slc : slice
+        Slice object representing the calibration region in the k-space data.
+
+    Use this function when your input still has the calibration region, such as:
+
+    o o o o o o o o o o o o o
+    . . . . . . . . . . . . .
+    o o o o o o o o o o o o o
+    . . . . . . . . . . . . .
+    o o o o o o o o o o o o o
+    . . . . o o o o . . . . .
+    o o o o o o o o o o o o o
+    . . . . o o o o . . . . .
+    o o o o o o o o o o o o o
+    . . . . . . . . . . . . .
+    o o o o o o o o o o o o o
+    . . . . . . . . . . . . .
+    o o o o o o o o o o o o o
+    """
+
+    # Sampled points
+    samp = (data.abs().norm(dim=0) > 0).to(torch.bool)
+
+    im_size = samp.shape
+    ndim = len(im_size)
+
+    # Conver endpoints to matrix slices
+    def get_slcs(css, ces, add_coil_dim=False):
+        slc = tuple([slice(css[i], ces[i] + 1) for i in range(ndim)])
+        if add_coil_dim:
+            slc = (slice(None),) + slc
+        return slc
+
+    # Check that data is fully sampled in selected cal region
+    def is_fs(css, ces):
+        return samp[get_slcs(css, ces)].all().item()
+
+    # intiial guess of calibration region as 3x3x3 center region
+    css = [im_size[i] // 2 - 1 for i in range(ndim)]
+    ces = [im_size[i] // 2 + 2 for i in range(ndim)]
+
+    for d in range(ndim):
+        # add left
+        while (css[d] > 0) and is_fs(css, ces):
+            css[d] -= 1
+        if not is_fs(css, ces):
+            css[d] += 1
+
+        # add right
+        while (ces[d] < im_size[d]) and is_fs(css, ces):
+            ces[d] += 1
+        if not is_fs(css, ces):
+            ces[d] -= 1
+
+    calib_slc = get_slcs(css, ces, add_coil_dim=True)
+
+    calib = data[calib_slc].clone()
+
+    return calib, calib_slc
 
 
 def grappa_mask(im_size: Tuple[int, ...], R: Tuple[int, ...]) -> torch.Tensor:
@@ -31,13 +106,14 @@ def grappa_mask(im_size: Tuple[int, ...], R: Tuple[int, ...]) -> torch.Tensor:
     nd = len(im_size)
     assert len(R) == nd, "R must have the same number of dimensions as im_size"
 
-    mask = torch.ones(im_size, dtype=torch.bool)
+    mask = torch.zeros(im_size, dtype=torch.long)
     for i in range(nd):
-        if R[i] > 1:
-            mask[
-                tuple(
-                    slice(None) if j != i else slice(0, None, R[i]) for j in range(nd)
-                )
-            ] = False
+        slc = tuple(
+            slice(None) if j != i else slice(0, im_size[i], R[i]) for j in range(nd)
+        )
+        mask[slc] += 1
+
+    # Only keep intersection
+    mask = mask == nd
 
     return mask

tests/test_grappa_3d.py
@@ -1,4 +1,3 @@
-from math import prod, sqrt
 from time import perf_counter
 
 import torch
@@ -12,21 +11,17 @@ from torch_grappa.utils import fft, ifft
 
 # Parameters
 device = torch.device(0)  # GPU device to use
-R = (1, 3, 1)
-kernel_size = (5, 4, 1)
-Ncal = 25  # center region width for calib
+R = (1, 3, 2)
+kernel_size = (5, 4, 2)
+Ncal = (25, 25, 25)  # center region width for calib
 noise_std = 1e-4
-dataset = "brain"  # head or brain
+dataset = "head"  # head or brain
 lamda_tik = 1e-6  # Tikhonov regularization parameter
 
 if dataset == "head":
-    data_path = (
-        "/local_mount/space/mayday/data/users/zachs/grappa/tests/data/im_3d_head.pt"
-    )
+    data_path = "data/im_3d_head.pt"
 elif dataset == "brain":
-    data_path = (
-        "/local_mount/space/mayday/data/users/zachs/grappa/tests/data/im_3d_brain.pt"
-    )
+    data_path = "data/im_3d_brain.pt"
 else:
     raise ValueError("Unknown dataset. Use 'head' or 'brain'.")
 
@@ -40,30 +35,36 @@ im_size = img.shape
 # create sampling mask
 samp_mask = grappa_mask(im_size, R).to(device)
 
-# Simulate acquisition
+"""
+Simulate acquisition
+"""
 x = img[None,] * mps
 y = fft(x, im_size)
 y = y + torch.randn_like(y) * noise_std
 
-calib_slice = tuple(
+calib_slc = [
+    slice(im_size[i] // 2 - Ncal[i] // 2, im_size[i] // 2 + Ncal[i] // 2 + 1)
+    for i in range(len(im_size))
+]
+calib_slc_full = tuple(
     [
         slice(
             None,
         )
     ]
-    + [
-        slice(im_size[i] // 2 - Ncal // 2, im_size[i] // 2 + Ncal // 2)
-        for i in range(len(im_size))
-    ]
+    + calib_slc
 )
-calib = y[calib_slice]
+calib_slc = tuple(calib_slc)
+calib = y[calib_slc_full]
 
+# Undersampling with calibration present
 y_us = y * samp_mask[None,]
+y_us[calib_slc_full] = calib
 
 tstart = perf_counter()
 print(f"Starting grappa with Rx={R[0]}, Ry={R[1]}, Rz={R[2]}.")
 
-y_grappa = grappa(y_us, calib, R, kernel_size, lamda_tik=lamda_tik)
+y_grappa = grappa(y_us, R, kernel_size, lamda_tik=lamda_tik)
 
 print(f"Grappa took {perf_counter() - tstart:.2f} seconds")
 
@@ -74,7 +75,7 @@ nrmse = (x_grappa - x).abs().norm() / x.abs().norm()
 print(f"NRMSE: {nrmse.item():.4f}")
 
 img_rec = x.abs().norm(dim=0)
-img_us = x_us.abs().norm(dim=0) * sqrt(prod(R))
+img_us = x_us.abs().norm(dim=0)  # * sqrt(prod(R))
 img_grappa = x_grappa.abs().norm(dim=0)
 
 # Visualization